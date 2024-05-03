a8e9e60f88fa3ff6f5112b7f8c0eee20e998d9f2
Vidhi03082003 <97527395+Vidhi03082003@users.noreply.github.com>
2024-05-03 13:07:41 +0530

    change

--- socket/socket.js
+++ socket/socket.js
@@ -8,7 +8,7 @@ const server=http.createServer(app);
 
 const io=new Server(server,{
     cors:{
-        origin:["http://localhost:3000"],
+        origin:["https://66348b978c951bb75002327b--majestic-puppy-d558ad.netlify.app/"],
         methods:["GET","POST"]
     }
 })
